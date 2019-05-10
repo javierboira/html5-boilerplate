fc9b422dea34ced4eb34c41158efc0498fc641e2
Javier Boira <49265130+javierboira@users.noreply.github.com>
2019-05-10 09:53:31 +0200

    Update gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-import gulp from 'gulp';
+import gulp from 'gulp';xbcvb
 
 // Load all gulp plugins automatically
 // and attach them to the `plugins` object
